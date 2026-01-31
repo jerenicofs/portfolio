1ad80d67f3f0fbd01a5bf621167d9609ef7138b4
Jerenico Franssen Semanuel <jerenicofs@gmail.com>
2026-01-31 14:45:55 +0700

    feat: cv and year update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,8 @@ const Footer = () => {
 
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light mb-8 md:mb-0">
-          &copy; 2024 Portfolio by Jerenico Franssen Semanuel
+          &copy; {new Date().getFullYear()} Portfolio by Jerenico Franssen
+          Semanuel
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
